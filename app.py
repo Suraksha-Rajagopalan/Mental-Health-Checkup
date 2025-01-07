73c60f944cfcf77a56450249f44b07622b05ebb9
Suraksha Rajagopalan <surakshagopalan@gmail.com>
2025-01-07 13:42:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import joblib
-from flask import Flask, request, jsonify,  render_template
+from flask import Flask, request, jsonify,  render_template, redirect, url_for, session, flash
 from sklearn.preprocessing import LabelEncoder
 import pickle
 import numpy as np
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Email, ValidationError
+import bcrypt
+from flask_mysqldb import MySQL
+from wtforms import StringField,PasswordField,SubmitField
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 # Load the pre-trained model
 model_path = 'D:/MINU/Projects/mental_health/best_random_forest_model.pkl'  
@@ -18,10 +23,105 @@ age_group_df = pd.read_csv(age_group_file)
 # Initialize LabelEncoder
 label_encoder = LabelEncoder()
 
-# Home Route
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'mydatabase'
+app.secret_key = 'your_secret_key_here'
+
+mysql = MySQL(app)
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name",validators=[DataRequired()])
+    email = StringField("Email",validators=[DataRequired(), Email()])
+    password = PasswordField("Password",validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+    def validate_email(self,field):
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM users where email=%s",(field.data,))
+        user = cursor.fetchone()
+        cursor.close()
+        if user:
+            raise ValidationError('Email Already Taken')
+
+class LoginForm(FlaskForm):
+    email = StringField("Email",validators=[DataRequired(), Email()])
+    password = PasswordField("Password",validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+
+#Hopme Page
 @app.route('/')
-def home():
-    return render_template("index.html")
+def index():
+    return render_template('index.html')
+
+#Register
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+
+        # store data into database 
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO users (name,email,password) VALUES (%s,%s,%s)",(name,email,hashed_password))
+        mysql.connection.commit()
+        cursor.close()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html',form=form)
+
+#Login
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE email=%s",(email,))
+        user = cursor.fetchone()
+        cursor.close()
+        if user and bcrypt.checkpw(password.encode('utf-8'), user[3].encode('utf-8')):
+            session['user_id'] = user[0]
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Login failed. Please check your email and password")
+            return redirect(url_for('login'))
+
+    return render_template('login.html',form=form)
+
+# Dashboard
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' in session:
+        user_id = session['user_id']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM users where id=%s",(user_id,))
+        user = cursor.fetchone()
+        cursor.close()
+
+        if user:
+            return render_template('dashboard.html',user=user)
+            
+    return redirect(url_for('login'))
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash("You have been logged out successfully.")
+    return redirect(url_for('login'))
+
 
 # Prediction Route
 @app.route('/predict', methods=['POST'])
